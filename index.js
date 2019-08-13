d2c965b0146345f0a561fb3b571eb2dc8db822e1
JessicaA-CodeCrew <ms.jessica.avarie@gmail.com>
2019-08-13 13:04:13 -0500

    Array Exercise

---
+++ index.js
@@ -0,0 +1,29 @@
+//EX1
+let kidsNames = ["Nyla", "Antjuan", "Liana", "Kyler"];
+alertMom(0, kidsNames);
+alertMom(1, kidsNames);
+alertMom(2, kidsNames);
+alertMom(3, kidsNames);
+
+console.log (kidsNames)
+function alertMom(placeInArray, kidsNames)
+{
+ alert(kidsNames[placeInArray]);
+}
+
+//EX2
+let arrayVariable = [1,2,3,4,5];
+for (let index in arrayVariable)
+{
+    console.log(index);
+    alert("Hello" + arrayVariable[index]);
+}
+
+//Ex3
+let grandParents = ["Jaunita", "Robert", "Richard", "Rose"]
+
+
+
+
+
+console.log(grandParents);
